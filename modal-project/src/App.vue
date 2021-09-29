7f06487d1aeb7af248c9ea92d1a7d58033b167a4
Eduardo <ealvarez@carminaticonsulting.com>
2021-09-29 18:36:47 -0500

    project init!

---
+++ modal-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'My First Vue.js App'
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
